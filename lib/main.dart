cf15c33f5dcb834717a5bff08ad6acf6b93e4121
nahyunuk <s24022@gsm.hs.kr>
2024-11-28 19:45:31 +0900

    feat : main 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_moive_api_rank/view/movie_rank_view.dart';
+import 'package:mvvm_moive_api_rank/viewModel/movie_rank_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: ChangeNotifierProvider<MovieRankViewModel>(
+        create: (context) => MovieRankViewModel(),
+        child: MovieRankView(),
+      ),
+    );
   }
 }
